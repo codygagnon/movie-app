96c22b07d706637ab243f64ceec0108a95930fee
codygagnon <cody.a.gagnon@gmail.com>
2022-07-14 16:43:52 -0700

    changed styles

--- src/Search.js
+++ src/Search.js
@@ -2,7 +2,6 @@ const Search = ({setSearchValue, handleSearch}) => {
     return (
         <div className="search">
             <input
-            className="search"
             type="text"
             placeholder="Enter a movie title"
             onChange={event => {setSearchValue(event.target.value)}} 
